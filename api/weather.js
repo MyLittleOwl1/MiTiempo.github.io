317e3a395a7fa05412a9fe12d090aebff84039c8
MyLittleOwl1 <mylittleowl1@protonmail.com>
2025-11-24 11:47:00 +0100

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+    // Configurar CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+    
+    // Manejar peticiones OPTIONS
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
     const { url } = req.query;
     const apiKey = req.headers['x-api-key'];
     
@@ -15,8 +25,12 @@ export default async function handler(req, res) {
         });
         
         const data = await response.json();
+        
+        // Copiar headers CORS en la respuesta
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(response.status).json(data);
     } catch (error) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(500).json({ error: error.message });
     }
 }
